d8abb20c23288be831256e7d91c258525a10c252
kasi viswanath <kasi.kantheti@ext.doordash.com>
2025-03-24 23:51:40 +0530

    Client Scripts

---
+++ ClientScripts/DD_CL_BILL_LINE_ITEM_VALIDATION_V2.js
@@ -0,0 +1,76 @@
+ /**
+  *@NApiVersion 2.x
+  *@NScriptType ClientScript
+  */
+ define(['N/record', 'N/runtime'], function(record, runtime) {
+
+     function validateLine(context) {
+
+         function isvalid(str) {
+             if (str) {
+                 if (str != "" && str != undefined && str != null) {
+                     return true;
+                 } else {
+                     return false;
+                 }
+             } else {
+                 return false;
+             }
+
+         }
+
+         var currentRecord = context.currentRecord;
+         var sublistName = context.sublistId;
+         var sublistFieldName = context.fieldId;
+         if (sublistName === 'expense') {
+
+             var getAccountId = currentRecord.getCurrentSublistValue({
+                 sublistId: sublistName,
+                 fieldId: 'account'
+             });
+
+             var getAmortizationSchedule = currentRecord.getCurrentSublistValue({
+                 sublistId: sublistName,
+                 fieldId: 'amortizationsched'
+             });
+
+             var getAmortizationStartDate = currentRecord.getCurrentSublistValue({
+                 sublistId: sublistName,
+                 fieldId: 'amortizstartdate'
+             });
+
+             var getAmortizationEndDate = currentRecord.getCurrentSublistValue({
+                 sublistId: sublistName,
+                 fieldId: 'amortizationenddate'
+             });
+
+
+             if (getAccountId == '247' || getAccountId == '1805' || getAccountId == '246') {
+
+                 if (isvalid(getAmortizationSchedule) && isvalid(getAmortizationStartDate) && isvalid(getAmortizationEndDate)) {
+                     return true;
+                 } else {
+                     if (confirm('You are recording to a prepaid line and it is missing an amortization schedule and/or start and end date, are you sure you want to proceed?')) {
+                         return true;
+                     } else {
+                         return false;
+                     }
+
+                 }
+
+             } else {
+                 return true;
+             }
+
+         }
+ else {
+                 return true;
+             }
+
+     }
+
+
+     return {
+         validateLine: validateLine
+     };
+ });
